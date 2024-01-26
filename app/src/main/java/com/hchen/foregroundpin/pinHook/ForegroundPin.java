dee2dd3c213e70189cead0ced734e0bdc916dd2f
焕晨HChen <HChen_LXi@outlook.com>
2024-01-26 16:49:28 +0800

    整理代码，添加许可头

--- app/src/main/java/com/hchen/foregroundpin/pinHook/ForegroundPin.java
+++ app/src/main/java/com/hchen/foregroundpin/pinHook/ForegroundPin.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ForegroundPin.
+
+ * ForegroundPin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ * Copyright (C) 2023-2024 ForegroundPin Contributions
+ */
 package com.hchen.foregroundpin.pinHook;
 
 import android.util.Log;
@@ -56,78 +74,6 @@ public class ForegroundPin extends Hook {
                     }
             );
 
-            /*findAndHookMethod("com.android.server.wm.MiuiFreeformPinManagerService",
-                    "hideStack", "com.android.server.wm.MiuiFreeFormActivityStack",
-                    new HookAction() {
-                        @Override
-                        protected void before(MethodHookParam param) throws Throwable {
-                            Object stack = param.args[0];
-                            Object mainWindow;
-                            Object mController = XposedHelpers.getObjectField(param.thisObject, "mController");
-                            Object mGestureListener = XposedHelpers.getObjectField(mController, "mGestureListener");
-                            Object mGestureAnimator = XposedHelpers.getObjectField(mGestureListener, "mGestureAnimator");
-                            XposedHelpers.callMethod(mGestureAnimator, "hideStack", stack);
-                            XposedHelpers.callMethod(mGestureAnimator, "applyTransaction");
-                            if (stack != null) {
-                                Object mTask = XposedHelpers.getObjectField(stack, "mTask");
-                                if (mTask != null) {
-                                    SurfaceControl surfaceControl;
-                                    if (mTask != null && (surfaceControl =
-                                            (SurfaceControl) XposedHelpers.getObjectField(mTask,
-                                                    "mSurfaceControl")) != null
-                                            && surfaceControl.isValid()) {
-                                        SurfaceControl.Transaction transaction = new SurfaceControl.Transaction();
-//                                        XposedHelpers.callMethod(transaction, "setCrop", surfaceControl, null);
-                                        XposedHelpers.callMethod(transaction, "hide", surfaceControl);
-                                        XposedHelpers.callMethod(transaction, "apply");
-                                        logE(tag, "moveTaskToBack: s: " + surfaceControl);
-                                    }
-                                }
-                            }
-                            Object activityRecord = XposedHelpers.getObjectField(stack, "mLastIconLayerWindowToken");
-                            if (activityRecord != null) {
-                                if (XposedHelpers.getObjectField(activityRecord, "mFloatWindwoIconSurfaceControl") != null &&
-                                        (mainWindow = XposedHelpers.callMethod(activityRecord, "findMainWindow")) != null) {
-                                    XposedHelpers.callMethod(mGestureAnimator, "setAlphaInTransaction",
-                                            XposedHelpers.getObjectField(mainWindow, "mSurfaceControl"), 1.0f);
-                                    XposedHelpers.callMethod(mGestureAnimator, "applyTransaction");
-                                }
-                            }
-                            XposedHelpers.callMethod(param.thisObject, "clearMiuiFreeWindowFloatIconLayer", stack);
-                            boolean isInMiniFreeFormMode = (boolean) XposedHelpers.callMethod(stack, "isInMiniFreeFormMode");
-                            if (!isInMiniFreeFormMode) {
-                                XposedHelpers.callMethod(mGestureAnimator, "removeAnimationControlLeash", stack);
-                            } else {
-                                XposedHelpers.callMethod(mGestureAnimator, "setWindowCropInTransaction", stack, null);
-                            }
-                            XposedHelpers.callMethod(stack, "updateCornerRadius");
-                            XposedHelpers.setObjectField(stack, "mIsRunningPinAnim", false);
-                            Object service = XposedHelpers.getObjectField(mController, "mMiuiFreeFormManagerService");
-                            Object mActivityTaskManagerService = XposedHelpers.getObjectField(service, "mActivityTaskManagerService");
-                            Object getInstance = XposedHelpers.callStaticMethod(findClassIfExists("android.app.ActivityTaskManager"),
-                                    "getInstance");
-                            List<ActivityManager.RunningTaskInfo> tasks = (List<ActivityManager.RunningTaskInfo>)
-                                    XposedHelpers.callMethod(getInstance, "getTasks", 1, false);
-                            if (!tasks.isEmpty()) {
-                                ActivityManager.RunningTaskInfo runningTaskInfo = tasks.get(0);
-                                int taskId = runningTaskInfo.taskId;
-                                Object nextFocusdTask = null;
-                                Object curentTask = XposedHelpers.callMethod(XposedHelpers.getObjectField(mActivityTaskManagerService,
-                                                "mRootWindowContainer"),
-                                        "anyTaskForId", taskId, 0);
-                                if (curentTask != null && (boolean) XposedHelpers.callMethod(curentTask, "isFocused")
-                                        && (nextFocusdTask = XposedHelpers.callMethod(curentTask, "getNextFocusableTask", false)) != null) {
-                                    XposedHelpers.callMethod(mActivityTaskManagerService, "setFocusedRootTask",
-                                            XposedHelpers.callMethod(nextFocusdTask, "getRootTaskId"));
-                                }
-                                logE(tag, "run: " + runningTaskInfo + " id: " + taskId + " cur: " + curentTask + " next: " + nextFocusdTask);
-                            }
-                            logE(tag, "run here");
-                            param.setResult(null);
-                        }
-                    }
-            );*/
-
             hookAllConstructors("com.android.server.wm.Task",
                     new HookAction() {
                         @Override
@@ -184,32 +130,6 @@ public class ForegroundPin extends Hook {
                         }
                     }
             );
-
-            /*findAndHookMethod("com.android.server.wm.MiuiFreeFormManagerService",
-                    "updatePinFloatingWindowPos",
-                    Rect.class, int.class, boolean.class, new HookAction() {
-                        @Override
-                        protected void before(MethodHookParam param) throws Throwable {
-                            if ((boolean) param.args[2]) {
-                                Object mffas = callMethod(param.thisObject, "getMiuiFreeFormActivityStackForMiuiFB", param.args[1]);
-                                Object mMiuiFreeFormGestureController = getObjectField(
-                                        getObjectField(
-                                                getObjectField(
-                                                        param.thisObject,
-                                                        "mActivityTaskManagerService"),
-                                                "mWindowManager"),
-                                        "mMiuiFreeFormGestureController");
-                                Object mGestureAnimator = getObjectField(
-                                        getObjectField(mMiuiFreeFormGestureController,
-                                                "mGestureListener"),
-                                        "mGestureAnimator");
-                                callMethod(getObjectField(mffas, "mLastIconLayerWindowToken"), "setVisibility", false);
-                                callMethod(mGestureAnimator, "hideStack", mffas);
-                                callMethod(mGestureAnimator, "applyTransaction");
-                            }
-                        }
-                    }
-            );*/
 
             findAndHookMethod("com.android.server.wm.MiuiFreeFormGestureController",
                     "moveTaskToFront",
