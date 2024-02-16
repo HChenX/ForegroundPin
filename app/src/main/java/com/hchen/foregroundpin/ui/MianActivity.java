dfd0b26eba394dd7ccd81ef409c5c3b9feb4a2ba
HChen <HChen_LXi@outlook.com>
2024-02-16 21:02:27 +0800

    基础Ui

---
+++ app/src/main/java/com/hchen/foregroundpin/ui/MianActivity.java
@@ -0,0 +1,17 @@
+package com.hchen.foregroundpin.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hchen.foregroundpin.R;
+
+public class MianActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
